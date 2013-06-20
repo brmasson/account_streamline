ab6e799d969e6177373c6681f5134a76c09c2c07
Alexandre Allouche <none@none>
2013-06-20 18:40:07 +0200

    ADDED transaction analyis control on account.move post method

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from . import account
+from . import account_move
 from . import account_move_line
 from . import partner
 from . import account_voucher_line
 from . import payment_selection
-from . import account_move
