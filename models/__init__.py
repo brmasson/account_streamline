dbe3d861260c1e4919ca25581c883edff5a7de6b
alorimier <anael.lorimier@xcg-consulting.fr>
2013-08-05 15:08:38 +0200

    Fix bug when posting account_voucher in the case voucher line were in company currency Voucher computation considered a 100% exchange difference instead of ignoring the foreign amount computation.

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from . import account
 from . import account_move
 from . import account_move_line
 from . import partner
+from . import account_voucher
 from . import account_voucher_line
 from . import payment_selection
 from . import res_user
