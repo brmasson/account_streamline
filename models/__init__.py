f7391fe31118bf0fd41ddef06d132d3af7db84c3
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2013-11-27 10:23:14 +0100

    Add a ledger_type utility model

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from . import account_move_line
 from . import partner
 from . import account_voucher
 from . import account_voucher_line
+from . import ledger_type
 from . import payment_selection
 from . import payment_suggestion
 from . import report
